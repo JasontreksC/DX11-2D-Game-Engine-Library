38f23e9e68830c851b4bbfef51a6eacb58d3f556
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2022-09-22 09:52:59 +0900

    Add files via upload

---
+++ BuildThisLibrary/SoundMgr.cpp
@@ -0,0 +1,74 @@
+#include "pch.h"
+#include "SoundMgr.h"
+
+bool SoundMgr::Init()
+{
+	FMOD_RESULT fr = FMOD::System_Create(&fmod_System);
+	if (fr != FMOD_OK)
+	{
+		MessageBox(nullptr, L"사운드 시스템 생성 실패", L"오류", 0);
+		return false;
+	}
+	fr = fmod_System->init(32, FMOD_INIT_NORMAL, 0);
+	if (fr != FMOD_OK)
+	{
+		MessageBox(nullptr, L"사운드 시스템 초기화 실패", L"오류", 0);
+		return false;
+	}
+	return true;
+}
+
+void SoundMgr::Update()
+{
+	fmod_System->update();
+}
+
+bool SoundMgr::AddFmodSound(string _fileName, string _instanceName, FMOD_MODE _loopmode)
+{
+	FMOD::Sound* newSound;
+	FMOD_RESULT fr = fmod_System->createSound(_fileName.c_str(), _loopmode, nullptr, &newSound);
+	if (fr != FMOD_OK)
+	{
+		MessageBox(nullptr, L"사운드 추가 실패", L"오류", 0);
+		return false;
+	}
+	soundList.insert(make_pair(_instanceName.c_str(), newSound));
+	return true;
+}
+
+bool SoundMgr::PlayFmodSound(string _instanceName)
+{
+	FMOD_RESULT fr = fmod_System->playSound(soundList.find(_instanceName.c_str())->second, nullptr, false, &fmod_Channel);
+	if (fr != FMOD_OK)
+	{
+		MessageBox(nullptr, L"사운드 재생 실패", L"오류", 0);
+		return false;
+	}
+	static float volume = 0.5f;
+	fmod_Channel->setVolume(volume);
+
+	return true;
+}
+
+void SoundMgr::StopFmodSound(string _instanceName)
+{
+	float getVolume = 0;
+	fmod_Channel->getVolume(&getVolume);
+
+	if (getVolume > 0.1f)
+	{
+		getVolume -= 0.1f;
+		fmod_Channel->setVolume(getVolume);
+	}
+	if (getVolume <= 0.1f)
+		soundList.find(_instanceName.c_str())->second->release();
+}
+
+void SoundMgr::Release()
+{
+	for (auto sound : soundList)
+	{
+		sound.second->release();
+	}
+	fmod_System->close();
+}
